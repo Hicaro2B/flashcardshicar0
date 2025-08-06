fb75a0c4f6e7639bfa8a5a6c92f7e9ef6115b54f
Hicaro2B <hicaro.munhoz@escola.pr.gov.br>
2025-08-06 12:20:20 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,27 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
+    '',
+    'matematica?',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'Geografia',
+    'geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
-criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
